1cddd8fd2242d2d1ccd1a725f38adf5525dae55f
FlackCode <fl4ckc0de@gmail.com>
2024-08-23 21:56:42 +0300

    Added secondary camera to fix weapon clipping & added UI!

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -88,10 +88,6 @@ public class Weapon : MonoBehaviour
                 burstBulletsLeft = bulletsPerBurst;
                 FireWeapon();
             }
-
-            if (AmmoManager.Instance.ammoDisplay != null) {
-                AmmoManager.Instance.ammoDisplay.text = $"{bulletsLeft/bulletsPerBurst}/{magazineSize/bulletsPerBurst}";
-            }
         }
     }
 
